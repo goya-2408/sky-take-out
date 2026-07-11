dfb601f8ed7bb260df2ab5bf28002eb6e0e5436f
goya-2408 <2133544159@qq.com>
2026-07-11 20:06:20 +0800

    编辑员工信息代码

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -8,8 +8,10 @@ import com.sky.result.PageResult;
 
 public interface EmployeeService {
 
+
     /**
      * 员工登录
+     *
      * @param employeeLoginDTO
      * @return
      */
@@ -17,6 +19,7 @@ public interface EmployeeService {
 
     /**
      * 新增员工
+     *
      * @param employeeDTO
      */
     void save(EmployeeDTO employeeDTO);
@@ -24,6 +27,7 @@ public interface EmployeeService {
     /**
      *
      * 分页查询
+     *
      * @param employeePageQueryDTO
      * @return
      */
@@ -32,8 +36,23 @@ public interface EmployeeService {
 
     /**
      * 启用禁用员工账号
+     *
      * @param status
      * @param id
      */
     void startOrStop(Integer status, Long id);
+
+
+    /**
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
+    Employee getById(Long id);
+
+    /**
+     * 修改编辑员工信息
+     * @param employeeDTO
+     */
+    void update(EmployeeDTO employeeDTO);
 }
